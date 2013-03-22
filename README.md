b4aa1f34532a74a6cc05e95f9cf51a051edaeab1
TheWhisp <daavis.90@gmail.com>
2013-03-22 10:13:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+CyanogenMod 10 device configuration for Samsung Galaxy Mini 2 - GT-S6500 (NFC)
+
+Maintained by TheWhisp
+
+http://forum.xda-developers.com/showthread.php?t=2012939
---
+++ s6500.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/s6500/overlay
+
+include device/samsung/msm7x27a-common/msm7x27a.mk
+
+## NFC
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag
+
+## NFC permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+
+## splash screen
+PRODUCT_COPY_FILES += \
+    device/samsung/s6500/rootdir/GT-S6500.rle:root/GT-S6500.rle
+   
+$(call inherit-product, vendor/samsung/jena/blobs.mk)
+#$(call inherit-product, vendor/samsung/jena/blobs-nfc.mk)
